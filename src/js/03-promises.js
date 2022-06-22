2c4b46bdf120717e9d4864a276b946349b703a05
Mixasikus <mixasiikus@gmail.com>
2022-06-23 01:19:01 +0300

    C:/Program Files/Git/

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,14 +5,17 @@ const refs = {
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
 });
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  }
-  reject(`❌ Rejected promise ${position} in ${delay}ms`);
-}
-// console.log(createPromise());
+const createPromise = () => {
+  return new Promise((position, delay) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      position('привет');
+    }
+    delay('пока');
+  });
+};
+
 createPromise(2, 1500)
   .then(({ position, delay }) => {
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
