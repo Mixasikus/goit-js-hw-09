31f477d59339c2d3d6111553357fed0bdd689dcc
Mixasikus <mixasiikus@gmail.com>
2022-06-22 22:51:08 +0300

    l

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,22 @@
-// const refs = {
-//   form: document.querySelector('.form'),
-// };
+const refs = {
+  form: document.querySelector('.form'),
+};
 
-// refs.form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   console.log('отправил');
-// });
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-console.log('привет');
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+});
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  }
+  reject(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+// console.log(createPromise());
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
