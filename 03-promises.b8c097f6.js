40ebf9002c8eb73922c376517d6a6d19155f645b
Mixasikus <94913462+Mixasikus@users.noreply.github.com>
2022-06-23 11:46:04 +0000

    Deploying to gh-pages from @ Mixasikus/goit-js-hw-09@42b3568f5b8741bc9726dd60574b2b5ea01d05b2 🚀

--- 03-promises.b8c097f6.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector(".form")}).form.addEventListener("submit",(e=>{e.preventDefault()}));new Promise(((e,o)=>{Math.random()>.3&&e("привет"),o("пока")})).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.b8c097f6.js.map
